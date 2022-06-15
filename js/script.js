d73f90a3017b012fb3de447952cce019b423f23b
Patrick <48574377+Patorii@users.noreply.github.com>
2022-06-15 10:17:12 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-const btn = document.querySelector('.botao')
-const usuario = document.querySelector('[type=text]')
-const senha = document.querySelector('[type=password]')
-const form = document.querySelector('.login')
+const btn = document.querySelector('#botao')
+const usuario = document.querySelector('#txtUsuario')
+const senha = document.querySelector('#txtSenha')
+const form = document.querySelector('#formLogin')
 
 function verificadora(input, input2){
     var preenchido = false
